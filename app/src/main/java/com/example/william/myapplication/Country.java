c3ea9aaeb639993d44237f5d19e9eaf21c7dc336
Mohd Sanusi <sanusi@jenjobs.com>
2015-11-30 11:25:01 +0800

    add country array for spinner

--- app/src/main/java/com/example/william/myapplication/Country.java
+++ app/src/main/java/com/example/william/myapplication/Country.java
@@ -1,7 +1,12 @@
 package com.example.william.myapplication;
 
-/**
- * Created by william on 11/30/2015.
- */
 public class Country {
+    public int id;
+    public String name;
+    //public String abbr;
+
+    public Country( int id, String name ){
+        this.id = id;
+        this.name = name;
+    }
 }
