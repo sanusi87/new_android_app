00f890af0e8c350b3e90b53abb2fec2c977ba82c
Mohd Sanusi <sanusi@jenjobs.com>
2015-12-15 18:21:55 +0800

    update job notice, job seeking

---
+++ app/src/main/java/com/example/william/myapplication/SelectJobSeekingStatus.java
@@ -0,0 +1,57 @@
+package com.example.william.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class SelectJobSeekingStatus extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_job_seeking_status);
+
+        final ListView lv = (ListView) findViewById(R.id.listOfJobSeekingStatus);
+        final JobSeekingAdapter ja = new JobSeekingAdapter(this);
+        lv.setAdapter(ja);
+
+        Bundle extra = getIntent().getExtras();
+        // checked selected index
+        if( extra != null ){
+            JobSeekingStatus value = (JobSeekingStatus) extra.get("jobSeekingStatus");
+            int selectedIndex = ja.statuses.indexOf(value); // index of the selected value
+            if( selectedIndex != -1 ){
+                lv.setItemChecked(selectedIndex, true);
+            }
+        }
+
+        Button okButton = (Button)findViewById(R.id.okButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SparseBooleanArray a = lv.getCheckedItemPositions();
+                JobSeekingStatus values = (JobSeekingStatus) ja.getItem( a.keyAt(0) );
+                Log.e("jobseekingstatus", ""+values.name);
+                Intent intent = new Intent();
+                intent.putExtra("jobseekingstatus", values);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        Button cancelButton = (Button)findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(Activity.RESULT_CANCELED);
+                finish();
+            }
+        });
+    }
+}
