971aadb1ecf1d3f9b377ea1dbeac9cdf04360a0c
Mohd Sanusi <sanusi87@gmail.com>
2015-12-17 01:09:24 +0800

    add database version

--- app/src/main/java/com/example/william/myapplication/Jenjobs.java
+++ app/src/main/java/com/example/william/myapplication/Jenjobs.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 
 public class Jenjobs {
     public static final String DATABASE_NAME = "jenjobs";
+    public static final int DATABASE_VERSION = 1;
 
     /*
     "jobType": {
