7cea3d3c2d733ab74d16b83ef8266e08709122df
Mohd Sanusi <sanusi@jenjobs.com>
2015-12-21 10:25:05 +0800

    add confirmation activity

--- app/src/main/java/com/example/william/myapplication/Confirmation.java
+++ app/src/main/java/com/example/william/myapplication/Confirmation.java
@@ -1,13 +1,37 @@
 package com.example.william.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Confirmation extends ActionBarActivity {
+public class Confirmation extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        Button yes = (Button)findViewById(R.id.yesButton);
+        Button no = (Button)findViewById(R.id.noButton);
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("result", "ok");
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(Activity.RESULT_CANCELED);
+                finish();
+            }
+        });
     }
 }
