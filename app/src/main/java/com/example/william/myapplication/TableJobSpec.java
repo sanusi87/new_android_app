431aac941d61d9e8e926f495494e64afa9e3d4f2
Mohd Sanusi <sanusi@jenjobs.com>
2015-12-22 15:43:36 +0800

    add findById()

--- app/src/main/java/com/example/william/myapplication/TableJobSpec.java
+++ app/src/main/java/com/example/william/myapplication/TableJobSpec.java
@@ -1,6 +1,7 @@
 package com.example.william.myapplication;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -24,6 +25,15 @@ public class TableJobSpec extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //db.execSQL(SQL_DELETE_ENTRIES);
         //onCreate(db);
+    }
+
+    public JobSpec findById( int id ){
+        String[] _id = {String.valueOf(id)};
+        Cursor c = db.rawQuery("SELECT * FROM "+TableJobSpec.TABLE_NAME+" WHERE id=?", _id);
+        if( c.moveToFirst() ){
+            return new JobSpec(c.getInt(0), c.getString(1));
+        }
+        return null;
     }
 
 }
