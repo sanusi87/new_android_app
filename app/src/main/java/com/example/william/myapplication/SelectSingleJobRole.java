76e0411ec263281066f5723064e09a8ce2b47adc
Mohd Sanusi <sanusi@jenjobs.com>
2015-12-23 10:20:32 +0800

    remove template

--- app/src/main/java/com/example/william/myapplication/SelectSingleJobRole.java
+++ app/src/main/java/com/example/william/myapplication/SelectSingleJobRole.java
@@ -1,7 +1,46 @@
 package com.example.william.myapplication;
 
-/**
- * Created by william on 12/23/2015.
- */
-public class SelectSingleJobRole {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class SelectSingleJobRole extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_job_role);
+
+        int jobSpecId = 200;
+        Bundle extra = getIntent().getExtras();
+        if( extra != null ){
+            jobSpecId = extra.getInt("jobspecid");
+        }
+
+        final ListView lv = (ListView)findViewById(R.id.listOfJobRole);
+        final JobRoleAdapter ca = new JobRoleAdapter(getApplicationContext(), jobSpecId);
+        ca.setSingle(true);
+        lv.setAdapter(ca);
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                JobRole values = (JobRole) ca.getItem(position);
+
+                Intent intent = new Intent();
+                intent.putExtra("jobrole", values);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        Button okButton = (Button)findViewById(R.id.okButton);
+        Button cancelButton = (Button)findViewById(R.id.cancelButton);
+        okButton.setVisibility(View.GONE);
+        cancelButton.setVisibility(View.GONE);
+    }
 }
--- app/src/main/java/com/example/william/myapplication/SelectSingleJobSpec.java
+++ app/src/main/java/com/example/william/myapplication/SelectSingleJobSpec.java
@@ -1,7 +1,40 @@
 package com.example.william.myapplication;
 
-/**
- * Created by william on 12/23/2015.
- */
-public class SelectSingleJobSpec {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class SelectSingleJobSpec extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_job_spec);
+
+        final ListView lv = (ListView)findViewById(R.id.listOfJobSpec);
+        final JobSpecAdapter ca = new JobSpecAdapter(getApplicationContext());
+        ca.setSingle(true);
+        lv.setAdapter(ca);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                JobSpec spec = (JobSpec) ca.getItem(position);
+
+                Intent intent = new Intent();
+                intent.putExtra("jobspec", spec);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        Button okButton = (Button)findViewById(R.id.okButton);
+        Button cancelButton = (Button)findViewById(R.id.cancelButton);
+
+        okButton.setVisibility(View.GONE);
+        cancelButton.setVisibility(View.GONE);
+    }
 }
