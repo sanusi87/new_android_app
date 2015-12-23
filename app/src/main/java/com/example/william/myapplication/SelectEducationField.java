656d795d52affeeaa9a0d65fa6eda06d3662f1ed
Mohd Sanusi <sanusi@jenjobs.com>
2015-12-23 14:50:16 +0800

    update select activity

--- app/src/main/java/com/example/william/myapplication/SelectEducationField.java
+++ app/src/main/java/com/example/william/myapplication/SelectEducationField.java
@@ -1,13 +1,33 @@
 package com.example.william.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class SelectEducationField extends ActionBarActivity {
+public class SelectEducationField extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_education_field);
+
+        ListView lv = (ListView) findViewById(R.id.listView);
+        final EducationFieldAdapter elf = new EducationFieldAdapter(this);
+        elf.setSingle(true);
+        lv.setAdapter(elf);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                EducationField values = (EducationField) elf.getItem(position);
+                Intent intent = new Intent();
+                intent.putExtra("edufield", values);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/william/myapplication/SelectEducationLevel.java
+++ app/src/main/java/com/example/william/myapplication/SelectEducationLevel.java
@@ -1,13 +1,33 @@
 package com.example.william.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class SelectEducationLevel extends ActionBarActivity {
+public class SelectEducationLevel extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_education_level);
+
+        ListView lv = (ListView) findViewById(R.id.listView);
+        final EducationLevelAdapter ela = new EducationLevelAdapter(this);
+        ela.setSingle(true);
+        lv.setAdapter(ela);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                EducationLevel values = (EducationLevel) ela.getItem(position);
+                Intent intent = new Intent();
+                intent.putExtra("edulevel", values);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
