942ebccecd8540b2a2e13a0400441f840431b0eb
Mohd Sanusi <sanusi@jenjobs.com>
2016-01-04 16:12:02 +0800

    commented log

--- app/src/main/java/com/example/william/myapplication/UpdateSalary.java
+++ app/src/main/java/com/example/william/myapplication/UpdateSalary.java
@@ -3,7 +3,6 @@ package com.example.william.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,7 +25,7 @@ public class UpdateSalary extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 MyCurrency c = (MyCurrency) currenciesSpinner.getSelectedItem();
-                Log.e("currency", ""+c.name);
+                //Log.e("currency", ""+c.name);
                 Intent intent = new Intent();
                 intent.putExtra("the_text", salary.getText().toString());
                 intent.putExtra("the_currency", c);
