6d9403d02d1c1123fa2fab2ab4c39fbb8c5c88c3
Mohd Sanusi <sanusi@jenjobs.com>
2016-01-05 16:19:19 +0800

    add notice period activity

---
+++ app/src/main/java/com/example/william/myapplication/UpdateNoticePeriod.java
@@ -0,0 +1,13 @@
+package com.example.william.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class UpdateNoticePeriod extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_notice_period);
+    }
+}
