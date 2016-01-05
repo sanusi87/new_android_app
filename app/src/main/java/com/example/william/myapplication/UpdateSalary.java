b4104663c219bfea765f1b392d703859dcaac3e6
Tyler Teh <tyler.teh@jenjobs.com>
2016-01-05 10:25:31 +0800

    process intent and set result

--- app/src/main/java/com/example/william/myapplication/UpdateSalary.java
+++ app/src/main/java/com/example/william/myapplication/UpdateSalary.java
@@ -2,13 +2,13 @@ package com.example.william.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class UpdateSalary extends ActionBarActivity {
+public class UpdateSalary extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,19 +17,28 @@ public class UpdateSalary extends ActionBarActivity {
         CurrencyAdapter ca = new CurrencyAdapter(this);
         final Spinner currenciesSpinner = (Spinner)findViewById(R.id.currency);
         currenciesSpinner.setAdapter(ca);
+        final EditText salaryInput = (EditText)findViewById(R.id.salary);
 
-        final EditText salary = (EditText)findViewById(R.id.salary);
+        if( getIntent() != null ){
+            Bundle extra = getIntent().getExtras();
+            String salary = extra.getString("salary");
+            salaryInput.setText(salary);
+
+            MyCurrency c = (MyCurrency) extra.get("currency");
+            if( c != null ){
+                currenciesSpinner.setSelection( ca.findPosition(c.id) );
+            }
+        }
 
         Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MyCurrency c = (MyCurrency) currenciesSpinner.getSelectedItem();
-                //Log.e("currency", ""+c.name);
                 Intent intent = new Intent();
-                intent.putExtra("the_text", salary.getText().toString());
+                intent.putExtra("the_text", salaryInput.getText().toString());
                 intent.putExtra("the_currency", c);
-
+                setResult(Activity.RESULT_OK, intent);
                 finish();
             }
         });
