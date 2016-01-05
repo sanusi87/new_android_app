d0e7b5b77131c2f2f3d6577b80aa30598a697ded
Mohd Sanusi <sanusi@jenjobs.com>
2016-01-05 16:24:26 +0800

    add content

--- app/src/main/java/com/example/william/myapplication/UpdateNoticePeriod.java
+++ app/src/main/java/com/example/william/myapplication/UpdateNoticePeriod.java
@@ -1,13 +1,76 @@
 package com.example.william.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class UpdateNoticePeriod extends ActionBarActivity {
+import java.util.ArrayList;
+
+public class UpdateNoticePeriod extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_notice_period);
+
+        final EditText availability = (EditText)findViewById(R.id.availability);
+        final Spinner availabilityUnit = (Spinner)findViewById(R.id.availabilityUnit);
+        Button okButton = (Button)findViewById(R.id.okButton);
+        Button cancelButton = (Button)findViewById(R.id.cancelButton);
+
+        String[] units = getResources().getStringArray(R.array.availability_unit);
+        availabilityUnit.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, units));
+
+        if( getIntent() != null ){
+            Bundle extra = getIntent().getExtras();
+            String _availability = extra.getString("availability");
+            String _availabilityUnit = extra.getString("availabilityUnit");
+
+            availability.setText(_availability);
+
+            for(int i=0;i<units.length;i++){
+                if( _availabilityUnit != null && _availabilityUnit.equals( units[i] ) ){
+                    availabilityUnit.setSelection( i );
+                }
+            }
+        }
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredAvailability = availability.getText().toString();
+                String selectedAvailabilityUnit = availabilityUnit.getSelectedItem().toString();
+
+                ArrayList<String> errors = new ArrayList<>();
+                if( enteredAvailability.length() == 0 ){
+                    errors.add("Availability value is required!");
+                }
+
+                if( errors.size() == 0 ){
+                    Intent intent = new Intent();
+                    intent.putExtra("availability", enteredAvailability);
+                    intent.putExtra("availabilityUnit", selectedAvailabilityUnit);
+                    setResult(Activity.RESULT_OK, intent);
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(), TextUtils.join(", ", errors), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(Activity.RESULT_CANCELED);
+                finish();
+            }
+        });
     }
 }
