5fce3b1d872f2bc1c4f06e29a9b3148922f14d1f
Mohd Sanusi <sanusi@jenjobs.com>
2016-01-07 18:08:05 +0800

    update content

--- app/src/main/java/com/example/william/myapplication/JobDetails.java
+++ app/src/main/java/com/example/william/myapplication/JobDetails.java
@@ -4,6 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class JobDetails extends Activity {
 
@@ -14,24 +26,48 @@ public class JobDetails extends Activity {
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
-
         if( extras != null ){
-
+            int postId = extras.getInt("post_id");
+            String[] param = {Jenjobs.JOB_DETAILS+"/"+postId};
+            new GetJobRequest().execute(param);
         }else{
-
+            Toast.makeText(getApplicationContext(), "Job posting ID not found!", Toast.LENGTH_SHORT).show();
         }
+
+
     }
 
-    public class GetRequest extends AsyncTask<Void, Void, Object> {
+    public class GetJobRequest extends AsyncTask<String, Void, JSONObject> {
 
         @Override
-        protected Object doInBackground(Void... params) {
-            return null;
+        protected JSONObject doInBackground(String... params) {
+            JSONObject _response = null;
+
+            final HttpClient httpclient = new DefaultHttpClient();
+            final HttpGet httpget = new HttpGet(params[0]);
+            httpget.addHeader("Content-Type", "application/json");
+            httpget.addHeader("Accept", "application/json");
+
+            try {
+                HttpResponse _http_response = httpclient.execute(httpget);
+                HttpEntity _entity = _http_response.getEntity();
+                InputStream is = _entity.getContent();
+
+                String responseString = JenHttpRequest.readInputStreamAsString(is);
+                _response = JenHttpRequest.decodeJsonObjectString(responseString);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return _response;
         }
 
         @Override
-        protected void onPostExecute(final Object success) {
-
+        protected void onPostExecute(JSONObject success) {
+            Log.e("onPostEx", "" + success);
+            if( success != null ){
+                Log.e("onPostEx", ""+success);
+            }
         }
     }
 }
