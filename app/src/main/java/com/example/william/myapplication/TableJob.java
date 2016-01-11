cbc33709e2e7e7d31f11f7bc110a1ff3da41db08
Mohd Sanusi <sanusi@jenjobs.com>
2016-01-11 10:26:47 +0800

    delete method checks for usage first before delete

--- app/src/main/java/com/example/william/myapplication/TableJob.java
+++ app/src/main/java/com/example/william/myapplication/TableJob.java
@@ -2,23 +2,25 @@ package com.example.william.myapplication;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TableJob extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "job";
-    public static String SQL_CREATE_ENTRIES = "CREATE TABLE '"+TableJob.TABLE_NAME+"' (salary INTEGER, " +
-            "currency_id INTEGER, " +
-            "state_id TEXT, " +
-            "country_id TEXT, " +
-            "job_type_id TEXT)";
+    public static String SQL_CREATE_ENTRIES = "CREATE TABLE '"+TableJob.TABLE_NAME+"' (id INTEGER, " +
+            "title TEXT, " +
+            "company TEXT, " +
+            "job_data TEXT)";
     public static String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS '"+TableJob.TABLE_NAME+"'";
 
     public SQLiteDatabase db;
+    private Context context;
 
     public TableJob(Context context){
         super(context, Jenjobs.DATABASE_NAME , null, Jenjobs.DATABASE_VERSION);
         db = this.getReadableDatabase();
+        this.context = context;
     }
 
     @Override
@@ -38,10 +40,38 @@ public class TableJob extends SQLiteOpenHelper{
 
     public boolean updateJob(ContentValues cv){
         int affectedRows = db.update(TableJob.TABLE_NAME, cv, null, null);
-        if( affectedRows > 0 ){
-            return true;
+        return affectedRows > 0;
+    }
+
+    public boolean deleteJob( int post_id ){
+        // check if this job is used in any other tables or not
+        int used = 0;
+
+        TableBookmark tableBookmark = new TableBookmark(context);
+        TableApplication tableApplication = new TableApplication(context);
+        TableInvitation tableInvitation = new TableInvitation(context);
+
+        // check if used in bookmark
+        Cursor bookmarks = tableBookmark.getBookmark(post_id);
+        if( bookmarks.getCount() > 0 ){ used += 1; }
+
+        // check if used in application
+        Cursor applications = tableApplication.getApplication(post_id);
+        if( applications.getCount() > 0 ){ used += 1; }
+
+        // check if used in invitation
+        Cursor invitations = tableInvitation.getInvitation(post_id);
+        if( invitations.getCount() > 0 ){ used += 1; }
+
+        // if not used, then deleted it
+        if( used == 0 ){
+            String _id = String.valueOf(post_id);
+            String[] param = {_id};
+            int affectedRows = db.delete(TableJob.TABLE_NAME, "id=?", param);
+            return affectedRows > 0;
+        }else{
+            return false;
         }
-        return false;
     }
 
 }
