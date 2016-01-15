57498dc99c4e4da91cdbe85de95960af8f2efa12
Mohd Sanusi <sanusi@jenjobs.com>
2016-01-15 18:00:53 +0800

    add getSetting() method

--- app/src/main/java/com/example/william/myapplication/TableSettings.java
+++ app/src/main/java/com/example/william/myapplication/TableSettings.java
@@ -2,18 +2,19 @@ package com.example.william.myapplication;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TableSettings  extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "settings";
 
-    public static String SQL_CREATE_ENTRIES = "CREATE TABLE '"+TableSettings.TABLE_NAME
+    public static String SQL_CREATE_ENTRIES = "CREATE TABLE '"+TABLE_NAME
             +"' (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
             "setting_key TEXT, " +
             "setting_value TEXT, " +
             "updated_at NUMERIC);";
-    public static String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS '"+TableSettings.TABLE_NAME+"'";
+    public static String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS '"+TABLE_NAME+"'";
 
     public SQLiteDatabase db;
 
@@ -37,11 +38,8 @@ public class TableSettings  extends SQLiteOpenHelper {
         ContentValues cv = new ContentValues();
         cv.put("setting_key", key);
         cv.put("setting_value", value);
-        Long newId = db.insert(TableSettings.TABLE_NAME, null, cv);
-        if( newId != null ){
-            return true;
-        }
-        return false;
+        Long newId = db.insert(TABLE_NAME, null, cv);
+        return newId.intValue() > 0;
     }
 
     public void updateSettings(String key, String value){
@@ -49,6 +47,19 @@ public class TableSettings  extends SQLiteOpenHelper {
         cv.put("setting_key", key);
         cv.put("setting_value", value);
         cv.put("updated_at", Jenjobs.date(null, null,null));
-        db.update(TableSettings.TABLE_NAME, cv, "setting_key=?", new String[]{key});
+        db.update(TABLE_NAME, cv, "setting_key=?", new String[]{key});
+    }
+
+    public String getSetting(String key){
+        String strSQL = "SELECT setting_value FROM "+TABLE_NAME+" WHERE setting_key=?";
+        String[] args = {key};
+
+        String value = null;
+        Cursor c = db.rawQuery(strSQL, args);
+        if( c.moveToFirst() ){
+            value = c.getString(0);
+            c.close();
+        }
+        return value;
     }
 }
