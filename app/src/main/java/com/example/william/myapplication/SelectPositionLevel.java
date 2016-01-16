db85a950202f586ac32955f50af8ce5612611351
Mohd Sanusi <sanusi87@gmail.com>
2016-01-17 00:36:23 +0800

    recode

--- app/src/main/java/com/example/william/myapplication/SelectPositionLevel.java
+++ app/src/main/java/com/example/william/myapplication/SelectPositionLevel.java
@@ -6,7 +6,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.SparseBooleanArray;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -22,51 +24,72 @@ public class SelectPositionLevel extends Activity {
 
         final ListView lv = (ListView)findViewById(R.id.listOfPositionLevels);
         final PositionLevelAdapter ca = new PositionLevelAdapter(getApplicationContext());
+
+        boolean single = true;
+
+        ArrayList selectedPositionLevels = null;
+        Bundle extra = getIntent().getExtras();
+
+        if( extra != null ){
+            selectedPositionLevels = (ArrayList) extra.get("positionlevel");
+            single = extra.getBoolean("single");
+        }
+
+        ca.setSingle(single);
         lv.setAdapter(ca);
 
-        Bundle extra = getIntent().getExtras();
-        // checked selected index
-        if( extra != null ){
-            ArrayList<PositionLevel> selectedPositionLevels = (ArrayList<PositionLevel>) extra.get("positionlevel");
+        Button okButton = (Button)findViewById(R.id.okButton);
+        Button cancelButton = (Button)findViewById(R.id.cancelButton);
+
+        if( single ){
+            ((ViewGroup)okButton.getParent()).setVisibility(View.GONE);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent();
+                    intent.putExtra("positionlevel", (PositionLevel)ca.getItem(position));
+                    setResult(Activity.RESULT_OK, intent);
+                    finish();
+                }
+            });
+        }else{
             if( selectedPositionLevels != null && selectedPositionLevels.size() > 0 ){
                 for(int i=0; i < selectedPositionLevels.size();i++){
-                    PositionLevel value = selectedPositionLevels.get(i);
+                    PositionLevel value = (PositionLevel) selectedPositionLevels.get(i);
                     int selectedIndex = ca.positionLevel.indexOf(value); // index of the selected value
                     if( selectedIndex != -1 ){
                         lv.setItemChecked(selectedIndex, true);
                     }
                 }
             }
-        }
 
-        Button okButton = (Button)findViewById(R.id.okButton);
-        okButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SparseBooleanArray a = lv.getCheckedItemPositions();
-                ArrayList<PositionLevel> values = new ArrayList<>();
-                for (int i = 0; i < a.size(); i++) {
-                    if (a.valueAt(i) && a.keyAt(i) >= 0) {
-                        PositionLevel c = (PositionLevel) ca.getItem(a.keyAt(i));
-                        values.add(c);
+            okButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SparseBooleanArray a = lv.getCheckedItemPositions();
+                    ArrayList<PositionLevel> values = new ArrayList<>();
+                    for (int i = 0; i < a.size(); i++) {
+                        if (a.valueAt(i) && a.keyAt(i) >= 0) {
+                            PositionLevel c = (PositionLevel) ca.getItem(a.keyAt(i));
+                            values.add(c);
+                        }
                     }
+
+                    Intent intent = new Intent();
+                    intent.putExtra("positionlevel", values);
+                    setResult(Activity.RESULT_OK, intent);
+                    finish();
                 }
+            });
 
-                Intent intent = new Intent();
-                intent.putExtra("positionlevel", values);
-                setResult(Activity.RESULT_OK, intent);
-                finish();
-            }
-        });
-
-        Button cancelButton = (Button)findViewById(R.id.cancelButton);
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setResult(Activity.RESULT_CANCELED);
-                finish();
-            }
-        });
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setResult(Activity.RESULT_CANCELED);
+                    finish();
+                }
+            });
+        }
     }
 
     @Override
