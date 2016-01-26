1621ab4d718bdec384ff0ba36eafd54902b9b1db
Mohd Sanusi <sanusi@jenjobs.com>
2016-01-26 14:56:22 +0800

    optimized imports

--- app/src/main/java/com/example/william/myapplication/JobSuggestion.java
+++ app/src/main/java/com/example/william/myapplication/JobSuggestion.java
@@ -1,21 +1,15 @@
 package com.example.william.myapplication;
 
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.FragmentTransaction;
-
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
