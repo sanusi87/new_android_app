14775b845fb4d13d77351a69e0e389a641ff7da6
Mohd Sanusi <sanusi87@gmail.com>
2016-01-31 22:41:57 +0800

    resize window

--- app/src/main/java/com/example/william/myapplication/SelectLanguageLevel.java
+++ app/src/main/java/com/example/william/myapplication/SelectLanguageLevel.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -39,5 +40,16 @@ public class SelectLanguageLevel extends Activity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // In order to not be too narrow, set the window size based on the screen resolution:
+        final int screen_width = getResources().getDisplayMetrics().widthPixels;
+        final int new_window_width = screen_width * 90 / 100;
+        WindowManager.LayoutParams layout = getWindow().getAttributes();
+        layout.width = Math.max(layout.width, new_window_width);
+        getWindow().setAttributes(layout);
     }
 }
