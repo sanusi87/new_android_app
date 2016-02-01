ac085e68f36573922e246276c24262e271547d96
Tyler Teh <tyler.teh@jenjobs.com>
2016-02-01 16:45:16 +0800

    show notice when no item

--- app/src/main/java/com/example/william/myapplication/JobSearchProfile.java
+++ app/src/main/java/com/example/william/myapplication/JobSearchProfile.java
@@ -2,7 +2,10 @@ package com.example.william.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class JobSearchProfile extends ActionBarActivity {
 
@@ -13,6 +16,13 @@ public class JobSearchProfile extends ActionBarActivity {
 
         ListView lv = (ListView) findViewById(R.id.listOfSearchProfile);
         JobSearchProfileAdapter jobSearchProfileAdapter = new JobSearchProfileAdapter(getApplicationContext());
+        jobSearchProfileAdapter.setActivity(this);
         lv.setAdapter(jobSearchProfileAdapter);
+
+        if( jobSearchProfileAdapter.getCount() == 0 ){
+            LinearLayout ll = (LinearLayout)findViewById(R.id.no_item);
+            ll.setVisibility(View.VISIBLE);
+            ((TextView)ll.findViewById(R.id.noticeText)).setText(R.string.no_job_search_profile);
+        }
     }
 }
