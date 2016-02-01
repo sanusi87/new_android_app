c12a0532d4293340157f526a7ec042341a10cb5b
Mohd Sanusi <sanusi@jenjobs.com>
2016-02-01 17:45:32 +0800

    pass access token to adapter

--- app/src/main/java/com/example/william/myapplication/JobSearchProfile.java
+++ app/src/main/java/com/example/william/myapplication/JobSearchProfile.java
@@ -1,5 +1,7 @@
 package com.example.william.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -9,14 +11,21 @@ import android.widget.TextView;
 
 public class JobSearchProfile extends ActionBarActivity {
 
+    SharedPreferences sharedPref;
+    static String accessToken = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_search_profile);
 
+        sharedPref = this.getSharedPreferences(MainActivity.JENJOBS_SHARED_PREFERENCE, Context.MODE_PRIVATE);
+        accessToken = sharedPref.getString("access_token", null);
+
         ListView lv = (ListView) findViewById(R.id.listOfSearchProfile);
         JobSearchProfileAdapter jobSearchProfileAdapter = new JobSearchProfileAdapter(getApplicationContext());
         jobSearchProfileAdapter.setActivity(this);
+        jobSearchProfileAdapter.setAccessToken(accessToken);
         lv.setAdapter(jobSearchProfileAdapter);
 
         if( jobSearchProfileAdapter.getCount() == 0 ){
