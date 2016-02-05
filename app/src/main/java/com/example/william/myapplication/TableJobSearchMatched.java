05a666e48cecbfde7469dff47ec731028fe1ba04
Tyler Teh <tyler.teh@jenjobs.com>
2016-02-05 15:56:50 +0800

    add content

--- app/src/main/java/com/example/william/myapplication/TableJobSearchMatched.java
+++ app/src/main/java/com/example/william/myapplication/TableJobSearchMatched.java
@@ -1,17 +1,19 @@
 package com.example.william.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TableJobSearchMatched extends SQLiteOpenHelper {
-    public static final String TABLE_NAME = "job";
-    public static String SQL_CREATE_ENTRIES = "CREATE TABLE '"+TableJob.TABLE_NAME+"' (id INTEGER, " +
+    public static final String TABLE_NAME = "job_matched";
+    public static String SQL_CREATE_ENTRIES = "CREATE TABLE '"+TABLE_NAME+"' (id INTEGER, " +
             "title TEXT, " +
             "company TEXT, " +
             "job_data TEXT, " +
             "date_closed NUMERIC)";
-    public static String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS '"+TableJob.TABLE_NAME+"'";
+    public static String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS '"+TABLE_NAME+"'";
 
     public SQLiteDatabase db;
     private Context context;
@@ -31,5 +33,18 @@ public class TableJobSearchMatched extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //db.execSQL(SQL_DELETE_ENTRIES);
         //onCreate(db);
+    }
+
+    public Long addJob(ContentValues cv){
+        return db.insert(TABLE_NAME, null, cv);
+    }
+
+    public Cursor getJob(){
+        String strSQL = "SELECT * FROM "+TABLE_NAME;
+        return db.rawQuery(strSQL, null);
+    }
+
+    public void truncateTable(){
+        db.execSQL("DELETE FROM "+TABLE_NAME);
     }
 }
