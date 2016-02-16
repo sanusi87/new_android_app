3cf78b28bdf1be82e10c53933374f83fb5b4b817
Mohd Sanusi <sanusi87@gmail.com>
2016-02-16 23:09:27 +0800

    resize window

--- app/src/main/java/com/example/william/myapplication/SelectJobMatcherDate.java
+++ app/src/main/java/com/example/william/myapplication/SelectJobMatcherDate.java
@@ -2,9 +2,9 @@ package com.example.william.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -31,5 +31,16 @@ public class SelectJobMatcherDate extends Activity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // In order to not be too narrow, set the window size based on the screen resolution:
+        final int screen_width = getResources().getDisplayMetrics().widthPixels;
+        final int new_window_width = screen_width * 90 / 100;
+        WindowManager.LayoutParams layout = getWindow().getAttributes();
+        layout.width = Math.max(layout.width, new_window_width);
+        getWindow().setAttributes(layout);
     }
 }
